d59c6f2065de92ce708dc14d65701e5ea3ee0c58
SI TAYEB <merwan.sitayeb@cgi.com>
2024-12-25 14:50:41 +0100

    first commit

---
+++ src/main/java/com/merhene/calprot_backend/CalprotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.merhene.calprot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalprotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalprotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/merhene/calprot_backend/CalprotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merhene.calprot_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalprotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
